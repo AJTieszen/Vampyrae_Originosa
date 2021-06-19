34e4ec486a242364b61b98bf15f741d00e6fe9ae
Andrew Tieszen <pilottzn@gmail.com>
2021-06-19 11:02:19 -0600

    Baseline code to render a window using SFML

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,22 @@
-// E2
+// Please make sure SFML Libs (see https://www.sfml-dev.org/) are included in your compiler toolchain
+// and SFML DLL files are in release folder.
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
 
 int main()
 {
-
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
 }
